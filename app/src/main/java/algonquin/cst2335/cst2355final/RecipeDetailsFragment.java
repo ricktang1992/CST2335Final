2bd95246bbceef826bf949b67e3db83279b1eb97
Ziyao Tang <tang0243@algonquinlive.com>
2023-11-19 00:15:56 -0500

    finished with image.!!!

--- app/src/main/java/algonquin/cst2335/cst2355final/RecipeDetailsFragment.java
+++ app/src/main/java/algonquin/cst2335/cst2355final/RecipeDetailsFragment.java
@@ -1,5 +1,6 @@
 package algonquin.cst2335.cst2355final;
 
+import android.app.Activity;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,10 +13,19 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.util.ArrayList;
 
 import algonquin.cst2335.cst2355final.databinding.RecipeInfoBinding;
 
@@ -31,7 +41,7 @@ public class RecipeDetailsFragment extends Fragment {
 
         RecipeInfoBinding binding = RecipeInfoBinding.inflate(inflater);
         binding.recipeInfoTitle.setText(selected.getTitle());
-        binding.recipeInfoSum.setText(selected.getImage());
+        //binding.recipeInfoSum.setText(selected.getImage());
         String iconUrl=selected.getImage();
         RequestQueue queue = null;
         queue = Volley.newRequestQueue(getActivity());
@@ -52,6 +62,22 @@ public class RecipeDetailsFragment extends Fragment {
         }, 1024, 1024, ImageView.ScaleType.CENTER, null, (error) -> {
             Log.e("ImageRequest", "Error loading image: " + error.getMessage());
         });
+        String stringURL="https://api.spoonacular.com/recipes/"
+                +selected.getWebsiteID()
+                +"/information?apiKey=670608d3fd1e4b15b120493cad68231a";
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, stringURL, null,
+                (response) -> {
+                    try {
+                        String summary=response.getString("summary");
+                        Log.d("WeatherResponse", "summary: " + summary);
+                        binding.recipeInfoSum.setText(summary);
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                },
+                (error) -> {
+                });
+        queue.add(request);
         queue.add(imgReq2);
 
         return binding.getRoot();
