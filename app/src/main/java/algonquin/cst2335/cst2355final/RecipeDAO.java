e269e75d44551da6da9f054c4bead0ed4c0b31b7
Ziyao Tang <tang0243@algonquinlive.com>
2023-11-21 15:07:13 -0500

    Revert "Part1 - Deezer Song"
    This reverts commit f858e316c04025a491ef0b2406f1b7e989bf4f95.

--- app/src/main/java/algonquin/cst2335/cst2355final/RecipeDAO.java
+++ app/src/main/java/algonquin/cst2335/cst2355final/RecipeDAO.java
@@ -1,5 +1,4 @@
 package algonquin.cst2335.cst2355final;
-
 import androidx.room.Dao;
 import androidx.room.Delete;
 import androidx.room.Insert;
@@ -8,13 +7,11 @@ import androidx.room.Query;
 import java.util.List;
 //test
 @Dao
-public interface DeezerSongDAO {
+public interface RecipeDAO {
     @Insert
-    public long insertSong(DeezerSong s);
-
-    @Query("Select * from DeezerSong;")
-    public List<DeezerSong> getAllSongs();
+    long insertRecipe(Recipe m);
+    @Query("Select * from Recipe")
+    List<Recipe> getAllRecipe();
     @Delete
-    void deleteSong(DeezerSong s);
-
+    void deleteRecipe(Recipe m);
 }
--- app/src/main/java/algonquin/cst2335/cst2355final/SongDatabase.java
+++
@@ -1,9 +0,0 @@
-package algonquin.cst2335.cst2355final;
-
-import androidx.room.Database;
-import androidx.room.RoomDatabase;
-
-@Database(entities = {DeezerSong.class}, version = 1)
-public abstract class SongDatabase extends RoomDatabase {
-    public abstract DeezerSongDAO dsDAO();
-}
