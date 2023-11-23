b35a4672bfe6e9e14bcd99cfccb9a8cdc8e2f92d
newsfeng <newsfeng0409@gmail.com>
2023-11-23 11:17:42 -0500

    up 11-23 11.17AM

--- app/src/main/java/algonquin/cst2335/cst2355final/Tianjiao/SunRoom.java
+++ app/src/main/java/algonquin/cst2335/cst2355final/Tianjiao/SunRoom.java
@@ -38,19 +38,19 @@ import algonquin.cst2335.cst2355final.databinding.SunRoomBinding;
 
 public class SunRoom extends AppCompatActivity {
     SunRoomBinding binding;
-    ArrayList<SunTerm> messages = new ArrayList<>();
+    ArrayList<SunTerm> texts1 = new ArrayList<>();
 
-    SunViewModel chatModel ;
+    SunViewModel sunModel ;
     SunTermDAO mDAO;
-    private RecyclerView.Adapter myAdapter;
+    private RecyclerView.Adapter sunAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
 
-        chatModel = new ViewModelProvider(this).get(SunViewModel.class);
-        chatModel.selectedMessage.observe(this, (selectedMessage) -> {
-            SunDetailsFragment newFragment = new SunDetailsFragment(selectedMessage);
+        sunModel = new ViewModelProvider(this).get(SunViewModel.class);
+        sunModel.selectedMessage.observe(this, (sundMessage) -> {
+            SunDetailsFragment newFragment = new SunDetailsFragment(sundMessage);
 
             FragmentManager fMgr = getSupportFragmentManager();
             FragmentTransaction tx = fMgr.beginTransaction();
@@ -64,17 +64,17 @@ public class SunRoom extends AppCompatActivity {
         SunDatabase db = Room.databaseBuilder(getApplicationContext(), SunDatabase.class, "databaseFileOnPhone").build();
         mDAO = db.sunTermDao();
 
-        messages = chatModel.messages.getValue();
-        if(messages == null)
+        texts1 = sunModel.messages.getValue();
+        if(texts1 == null)
         {
-            chatModel.messages.setValue(messages = new ArrayList<>());
+            sunModel.messages.setValue(texts1 = new ArrayList<>());
 
             Executor thread = Executors.newSingleThreadExecutor();
             thread.execute(() ->
             {
-                messages.addAll( mDAO.getAllSunTerms() ); //Once you get the data from database
+                texts1.addAll( mDAO.getAllSunTerms() ); //Once you get the data from database
 
-                runOnUiThread( () ->  binding.recyclerView.setAdapter( myAdapter )); //You can then load the RecyclerView
+                runOnUiThread( () ->  binding.recyclerView.setAdapter( sunAdapter )); //You can then load the RecyclerView
             });
         }
 
@@ -89,22 +89,28 @@ public class SunRoom extends AppCompatActivity {
 
         //SharedPreferences to save something about what was typed in the EditText for use the next time
         SharedPreferences prefs = getSharedPreferences("searchHistory", Context.MODE_PRIVATE);
-        AtomicReference<EditText> sunText = new AtomicReference<>(binding.editTextSearch);
+        AtomicReference<EditText> sunText = new AtomicReference<>(binding.editTextSearch );
+       AtomicReference<EditText> sunsText = new AtomicReference<>(binding.latitudeSearch );
+
 
         binding.btnSearch.setOnClickListener(clk -> {
 
             SharedPreferences.Editor editor = prefs.edit();
             editor.putString("sunText", sunText.get().getText().toString() );
+//            editor.putString("sunsText", sunsText.get().getText().toString() );
+
             editor.apply();
-            SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a");
-            String currentDateandTime = sdf.format(new Date());
+      //      SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a");
+      //      String currentDateandTime = sdf.format(new Date());
             String inputMessage = binding.editTextSearch.getText().toString();
-            SunTerm thisMessage = new SunTerm(inputMessage, currentDateandTime);
-            messages.add(thisMessage);
+//           String inputMessages = binding.latitudeSearch.getText().toString();
+
+            SunTerm thisMessage = new SunTerm(inputMessage,"00000");
+            texts1.add(thisMessage);
 
             // clear teh previous text
             binding.editTextSearch.setText("");
-            myAdapter.notifyDataSetChanged();
+            sunAdapter.notifyDataSetChanged();
 
             Executor thread = Executors.newSingleThreadExecutor();
             thread.execute(new Runnable() {
@@ -116,22 +122,22 @@ public class SunRoom extends AppCompatActivity {
             });
 
 
-            runOnUiThread(() ->{myAdapter.notifyItemInserted(messages.size() - 1);});
+            runOnUiThread(() ->{sunAdapter.notifyItemInserted(texts1.size() - 1);});
 
             binding.editTextSearch.setText("");
         });
 
         sunText.get().setText(prefs.getString("searchText",""));
-        chatModel.selectedMessage.observe(this, (newMessageValue) -> {
-            SunDetailsFragment dictionaryFragment = new SunDetailsFragment( newMessageValue );
+        sunModel.selectedMessage.observe(this, (newMessageValue) -> {
+            SunDetailsFragment sunframeLayout = new SunDetailsFragment( newMessageValue );
             FragmentManager fMgr = getSupportFragmentManager();
             FragmentTransaction tx = fMgr.beginTransaction();
-            tx.addToBackStack("");
-            tx.replace(R.id.sunframeLayout,dictionaryFragment);
+            tx.addToBackStack("back");
+            tx.replace(R.id.sunframeLayout,sunframeLayout);
             tx.commit();
         });
 
-        binding.recyclerView.setAdapter(myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
+        binding.recyclerView.setAdapter(sunAdapter = new RecyclerView.Adapter<MyRowHolder>() {
             @NonNull
             @Override
             public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -147,7 +153,7 @@ public class SunRoom extends AppCompatActivity {
 
             @Override
             public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
-                SunTerm obj = messages.get(position);
+                SunTerm obj = texts1.get(position);
                 holder.messageText.setText(obj.getTerm());
 //                holder.messageText.setText("");
                 holder.timeText.setText(obj.getTimeSent());
@@ -156,7 +162,7 @@ public class SunRoom extends AppCompatActivity {
 
             @Override
             public int getItemCount() {
-                return messages.size();
+                return texts1.size();
             }
         });
 
@@ -166,7 +172,7 @@ public class SunRoom extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         super.onCreateOptionsMenu(menu);
-        getMenuInflater().inflate(R.menu.yuxing_menu_file, menu);
+        getMenuInflater().inflate(R.menu.tianjiao_menu, menu);
         return true;
     }
 
@@ -180,7 +186,7 @@ public class SunRoom extends AppCompatActivity {
                 builder.setTitle("Delete Message")
                         .setPositiveButton("Yes", (dialog, cl) -> {
                             // Get the selected message
-                            SunTerm selectedMessage = chatModel.selectedMessage.getValue();
+                            SunTerm selectedMessage = sunModel.selectedMessage.getValue();
                             if (selectedMessage != null) {
                                 // Delete the message from the database
                                 Executors.newSingleThreadExecutor().execute(() -> {
@@ -188,8 +194,8 @@ public class SunRoom extends AppCompatActivity {
                                 });
 
                                 // Remove the message from the list and update the RecyclerView
-                                messages.remove(selectedMessage);
-                                myAdapter.notifyDataSetChanged();
+                                texts1.remove(selectedMessage);
+                                sunAdapter.notifyDataSetChanged();
 
                                 // Show a Snackbar with an undo option
                                 Snackbar.make(binding.getRoot(), "Message deleted", Snackbar.LENGTH_LONG)
@@ -200,8 +206,8 @@ public class SunRoom extends AppCompatActivity {
                                                 selectedMessage.id = id;
                                                 runOnUiThread(() -> {
                                                     // Add the message back to the list and update the RecyclerView
-                                                    messages.add(selectedMessage);
-                                                    myAdapter.notifyDataSetChanged();
+                                                    texts1.add(selectedMessage);
+                                                    sunAdapter.notifyDataSetChanged();
                                                 });
                                             });
                                         })
@@ -239,9 +245,9 @@ public class SunRoom extends AppCompatActivity {
 
             itemView.setOnClickListener(clk -> {
                 int position = getAbsoluteAdapterPosition();
-                SunTerm selected = messages.get(position);
+                SunTerm selected = texts1.get(position);
 
-                chatModel.selectedMessage.postValue(selected);
+                sunModel.selectedMessage.postValue(selected);
             });
             messageText = itemView.findViewById(R.id.message);
             timeText =itemView.findViewById(R.id.time);
