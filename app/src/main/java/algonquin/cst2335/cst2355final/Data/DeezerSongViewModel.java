23a0dbcb5926f444a0a89787119c926c97bd61f8
Yuxing Xu <xu000296@algonquinlive.com>
2023-12-06 10:27:24 -0500

    javadoc

--- app/src/main/java/algonquin/cst2335/cst2355final/Data/DeezerSongViewModel.java
+++ app/src/main/java/algonquin/cst2335/cst2355final/Data/DeezerSongViewModel.java
@@ -11,10 +11,38 @@ import java.util.List;
 
 import algonquin.cst2335.cst2355final.rita.DeezerSong;
 
+/**
+ * ViewModel class for managing DeezerSong data in the application.
+ *
+ * This class extends AndroidX ViewModel and is responsible for holding and managing the
+ * UI-related data in a lifecycle-conscious way. It is associated with the DeezerSongFragment
+ * and communicates with the underlying data source to provide data to the UI.
+ *
+ * This ViewModel contains MutableLiveData instances for observing changes in the list of songs,
+ * the currently selected song, and the list of favorite songs. These LiveData objects can be
+ * observed by UI components to update the UI when the underlying data changes.
+ *
+ * @see androidx.lifecycle.ViewModel
+ * @see androidx.lifecycle.MutableLiveData
+ * @see algonquin.cst2335.cst2355final.rita.DeezerSong
+ */
 public class DeezerSongViewModel extends ViewModel {
+
+    /**
+     * MutableLiveData for the list of Deezer songs. Observing this LiveData allows UI components
+     * to update when the list of songs changes.
+     */
     public MutableLiveData<ArrayList<DeezerSong>> songs = new MutableLiveData<>();
 
+    /**
+     * MutableLiveData for the currently selected Deezer song. Observing this LiveData allows UI
+     * components to update when the selected song changes.
+     */
     public MutableLiveData<DeezerSong> selectedSong = new MutableLiveData<>();
-    public MutableLiveData<ArrayList<DeezerSong>> favoriteSongsArray = new MutableLiveData<>();
 
+    /**
+     * MutableLiveData for the list of favorite Deezer songs. Observing this LiveData allows UI
+     * components to update when the list of favorite songs changes.
+     */
+    public MutableLiveData<ArrayList<DeezerSong>> favoriteSongsArray = new MutableLiveData<>();
 }
--- app/src/main/java/algonquin/cst2335/cst2355final/Data/RecipeViewModel.java
+++ app/src/main/java/algonquin/cst2335/cst2355final/Data/RecipeViewModel.java
@@ -7,7 +7,32 @@ import java.util.ArrayList;
 import algonquin.cst2335.cst2355final.ziyao.Recipe;
 
 
+/**
+ * ViewModel class for managing Recipe data in the application.
+ *
+ * This class extends AndroidX ViewModel and is responsible for holding and managing the
+ * UI-related data in a lifecycle-conscious way. It is associated with the RecipeFragment
+ * and communicates with the underlying data source to provide data to the UI.
+ *
+ * This ViewModel contains MutableLiveData instances for observing changes in the list of recipes
+ * and the currently selected recipe. These LiveData objects can be observed by UI components
+ * to update the UI when the underlying data changes.
+ *
+ * @see androidx.lifecycle.ViewModel
+ * @see androidx.lifecycle.MutableLiveData
+ * @see algonquin.cst2335.cst2355final.ziyao.Recipe
+ */
 public class RecipeViewModel extends ViewModel {
-    public MutableLiveData<ArrayList<Recipe>> recipes = new MutableLiveData< >();
+
+    /**
+     * MutableLiveData for the list of recipes. Observing this LiveData allows UI components
+     * to update when the list of recipes changes.
+     */
+    public MutableLiveData<ArrayList<Recipe>> recipes = new MutableLiveData<>();
+
+    /**
+     * MutableLiveData for the currently selected recipe. Observing this LiveData allows UI components
+     * to update when the selected recipe changes.
+     */
     public MutableLiveData<Recipe> selectedRecipe = new MutableLiveData<>();
 }
